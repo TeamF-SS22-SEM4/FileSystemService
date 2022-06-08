cf259cd2e5f0a4510a78ae8660018f326cf06bcc
Smightym8 <michaelspiegel@live.at>
2022-06-08 14:39:19 +0200

    Added some example mp3 files and added random choosing of files

--- src/main/java/at/fhv/ec/application/impl/FileServiceImpl.java
+++ src/main/java/at/fhv/ec/application/impl/FileServiceImpl.java
@@ -8,7 +8,9 @@ import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 @ApplicationScoped
 public class FileServiceImpl implements FileService {
@@ -16,10 +18,19 @@ public class FileServiceImpl implements FileService {
     Logger logger;
 
     private static final String FILEPATH = "/files/mp3/example.mp3";
+    private static final List<String> FILES = List.of(
+        "/files/mp3/example.mp3",
+        "/files/mp3/example2.mp3",
+        "/files/mp3/example3.mp3",
+        "/files/mp3/example4.mp3"
+    );
 
     @Override
     public byte[] getExampleFile() throws IOException {
-        File file = new File(FILEPATH);
+        Random random = new Random();
+        int randomPos = random.nextInt(FILES.size());
+
+        File file = new File(FILES.get(randomPos));
 
         if(!file.exists()) {
             logger.info("File not found");
